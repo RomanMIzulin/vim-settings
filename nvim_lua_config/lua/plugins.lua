b81cff85ef35c1a27db46b634a5be043a5574ec6
RomanMIzulin <dazyra@gmail.com>
2024-09-03 12:34:37 +0400

    Create plugins.lua

---
+++ nvim_lua_config/lua/plugins.lua
@@ -0,0 +1,75 @@
+return {
+
+  -- Telescope for fuzzy finding
+   {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  },
+
+  -- CHADtree
+   {'ms-jpq/chadtree', branch= 'chad', build= 'python3 -m chadtree deps'},
+  -- Treesitter for enhanced syntax highlighting
+   {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  },
+
+  -- LSP support
+   'neovim/nvim-lspconfig',
+
+  -- Pyright for Python LSP
+   'https://github.com/neovim/nvim-lspconfig',
+
+  -- Null-ls for integrating tools like black
+   'jose-elias-alvarez/null-ls.nvim',
+
+  -- LuaSnip for snippets
+   {
+    'L3MON4D3/LuaSnip',
+    	version = "v2.3", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp",
+    requires = {'saadparwaiz1/cmp_luasnip'},
+    dependencies = { "rafamadriz/friendly-snippets" }
+ },
+
+  -- nvim-cmp for autocompletion
+ --  'hrsh7th/nvim-cmp',
+ --  'hrsh7th/cmp-nvim-lsp',
+ --  'hrsh7th/cmp-buffer',
+ --  'hrsh7th/cmp-path',
+
+  -- Set up themes
+   'navarasu/onedark.nvim',
+   'morhetz/gruvbox',
+   { "rafamadriz/friendly-snippets" },
+{
+  "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+  lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+  dependencies = {
+    -- main one
+    { "ms-jpq/coq_nvim", branch = "coq" },
+
+    -- 9000+ Snippets
+    { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+    -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+    -- Need to **configure separately**
+    { 'ms-jpq/coq.thirdparty', branch = "3p" }
+    -- - shell repl
+    -- - nvim lua api
+    -- - scientific calculator
+    -- - comment banner
+    -- - etc
+  },
+  init = function()
+    vim.g.coq_settings = {
+        auto_start = true, -- if you want to start COQ at startup
+        -- Your COQ settings here
+    }
+  end,
+  config = function()
+    -- Your LSP settings here
+  end,
+}
+}
